e2661c5aed9cf7df7c1114f445cdf47289ea5963
Pedro Caliman <pcaliman@gmail.com>
2026-03-19 19:22:51 -0400

    Ajuste_Zoom
    Refactor isDesktopFromContext to use ResponsiveBreakpoints and handle web context.

--- lib/useful/is_desktop_from_context.dart
+++ lib/useful/is_desktop_from_context.dart
@@ -1,4 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-bool isDesktopFromContext(BuildContext context) =>
-    MediaQuery.of(context).size.width >= 1440;
+class ResponsiveBreakpoints {
+  static const double mobileMax = 599;
+  static const double tabletMax = 1023;
+  static const double desktopMin = 1024;
+}
+
+double _effectiveWidth(BuildContext context) {
+  final mediaQuery = MediaQuery.of(context);
+
+  if (kIsWeb) {
+    return mediaQuery.size.width * mediaQuery.devicePixelRatio;
+  }
+
+  return mediaQuery.size.width;
+}
+
+bool isDesktopFromContext(BuildContext context) {
+  final width = _effectiveWidth(context);
+  return width >= ResponsiveBreakpoints.desktopMin;
+}
